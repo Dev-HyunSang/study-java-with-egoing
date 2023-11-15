e9a61062ddba52995a18ab6e8e9dc4ed984c011d
HyunSang Park <me@hyunsang.dev>
2023-11-15 22:47:04 +0900

    updated commit

---
+++ src/ConstantDemon.java
@@ -0,0 +1,10 @@
+// 2023.11.13 - 생활코딩 Java - 상수형 데이터 타입
+public class ConstantDemon {
+    public static void main(String[] args) {
+        // float a = 2.2;
+        float a = 2.2F;
+        double b = 2.2;
+        long c = 2147483648L;
+        short d = 200;
+    }
+}
